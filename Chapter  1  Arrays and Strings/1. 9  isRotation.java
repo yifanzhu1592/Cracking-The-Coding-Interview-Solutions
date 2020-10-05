0289ba804d3163032aac7c43a4acf99c70e60746
Yifan Zhu <zhuyi@tcd.ie>
2020-10-05 22:17:02 +0100

    Create 1. 9  isRotation.java

---
+++ Chapter  1  Arrays and Strings/1. 9  isRotation.java
@@ -0,0 +1,10 @@
+boolean isRotation(String s1, String s2) {
+  int len = s1.length();
+  /* Check that s1 and s2 are equal length and not empty */
+  if (len == s2.length() && len > 0) {
+    /* Concatenate s1 and s1 within new buffer */
+    String s1s1 = s1 + s1;
+    return isSubstring(s1s1, s2);
+  }
+  return false;
+}
