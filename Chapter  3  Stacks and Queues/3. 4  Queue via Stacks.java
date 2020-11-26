34f45dfd9758c91ad17b3ed1f3673edd0d981848
Yifan Zhu <zhuyi@tcd.ie>
2020-11-26 21:17:06 +0000

    Create 3. 4  Queue via Stacks.java

---
+++ Chapter  3  Stacks and Queues/3. 4  Queue via Stacks.java
@@ -0,0 +1,37 @@
+public class MyQueue<T> {
+  Stack<T> stackNewest, stackOldest;
+  
+  public MyQueue() {
+    stackNewest = new Stack<T>();
+    stackOldest = new Stack<T>();
+  }
+  
+  public int size() {
+    return stackNewest.size() + stackOldest.size();
+  }
+  
+  public void add(T value) {
+    /* Push onto stackNewest, which always has the newest elements on top */
+    stackNewest.push(value);
+  }
+  
+  /* Move elements from stackNewest into stackOldest. This is usually done so that
+   * we can do operations on stackOldest. */
+  private void shiftStacks() {
+    if (stackOldest.isEmpty()) {
+      while (!stackNewest.isEmpty()) {
+        stackOldest.push(stackNewest.pop());
+      }
+    }
+  }
+  
+  public T peek() {
+    shiftStacks(); // Ensure stackOldest has the current elements
+    return stackOldest.peek(); // retrieve the oldest item.
+  }
+  
+  public T remove() {
+    shiftStacks(); // Ensure stackOldest has the current elements
+    return stackOldest.pop(); // pop the oldest item.
+  }
+}
