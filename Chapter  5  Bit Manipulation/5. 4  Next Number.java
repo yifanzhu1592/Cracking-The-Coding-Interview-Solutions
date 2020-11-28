ee27c7f60c2c150e462a0a4998b2257d7da52d1f
Yifan Zhu <zhuyi@tcd.ie>
2020-11-28 13:22:35 +0000

    Update 5. 4  Next Number.java

--- Chapter  5  Bit Manipulation/5. 4  Next Number.java
+++ Chapter  5  Bit Manipulation/5. 4  Next Number.java
@@ -26,3 +26,28 @@ int getNext(int n) {
   n |= (1 << (c1 - 1)) - 1; // Insert (c1-1) ones on the right.
   return n;
 }
+
+int getPrev(int n) {
+  int temp = n;
+  int c0 = 0;
+  int c1 = 0;
+  while (temp & 1 == 1) {
+    c1++;
+    temp >>= 1;
+  }
+  
+  if (temp == 0) return -1;
+  
+  while (((temp & 1) == 0) && (temp != 0)) {
+    c0++;
+    temp >>= 1;
+  }
+  
+  int p = c0 + c1; // position of rightmost non-trailing one
+  n &= ((~0) << (p + 1)); // clears from bit p onwards
+  
+  int mask = (1 << (c1 + 1)) - 1; // Sequence of (c1+1) ones
+  n |= mask << (c0 - 1);
+  
+  return n;
+}
