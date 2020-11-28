e2f389dc5a996a96aeee7cccf81f8226531b46f3
Yifan Zhu <zhuyi@tcd.ie>
2020-11-28 16:44:00 +0000

    Update 8. 4  Power Set.java

--- Chapter  8  Recursion and Dynamic Programming/8. 4  Power Set.java
+++ Chapter  8  Recursion and Dynamic Programming/8. 4  Power Set.java
@@ -7,3 +7,15 @@ ArrayList<ArrayList<Integer>> getSubsets2(ArrayList<Integer> set) {
   }
   return allsubsets;
 }
+
+ArrayList<Integer> convertIntToSet(int x, ArrayList<Integer> set) {
+  ArrayList<Integer> subset = new ArrayList<Integer>();
+  int index = 0;
+  for (int k = x; k > 0; k >>= 1) {
+    if ((k & 1) == 1) {
+      subset.add(set.get(index));
+    }
+    index++;
+  }
+  return subset;
+}
